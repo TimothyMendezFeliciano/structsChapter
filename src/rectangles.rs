7fe8cf97bb9c07fb3f8985a2fc3b76893cd79ffa
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-22 22:00:32 -0400

    Method Syntax done

--- src/rectangles.rs
+++ src/rectangles.rs
@@ -3,13 +3,26 @@ struct Rectangle {
     height: usize,
 }
 
+impl Rectangle {
+    fn area(&self) -> usize {
+        self.width * self.height
+    }
+}
+
 pub fn rectangles() {
     let rect1: Rectangle = Rectangle {
         width: 50,
         height: 30,
     };
 
+    let methodRect: Rectangle = Rectangle {
+        width: 87,
+        height: 132,
+    };
+
     println!("The area of the rectangle is {} square pixels", area(&rect1)); // Let the function borrow the struct, so that the main function retains ownership.
+
+    println!("The area of the rectangle with method syntax is {}", methodRect.area());
 }
 
 // This provides a more descriptive understanding of the code
