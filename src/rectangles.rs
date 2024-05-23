bd54c184dec4b6ffc5f48a994e3924daf632ddc4
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-22 22:09:12 -0400

    References in methos

--- src/rectangles.rs
+++ src/rectangles.rs
@@ -7,6 +7,10 @@ impl Rectangle {
     fn area(&self) -> usize {
         self.width * self.height
     }
+
+    fn can_hold(&self, rect2: &Rectangle) -> bool {
+        (self.height * self.width) >= (rect2.height * rect2.width)
+    }
 }
 
 pub fn rectangles() {
@@ -23,6 +27,9 @@ pub fn rectangles() {
     println!("The area of the rectangle is {} square pixels", area(&rect1)); // Let the function borrow the struct, so that the main function retains ownership.
 
     println!("The area of the rectangle with method syntax is {}", methodRect.area());
+
+    println!("Can rect1 hold rect2 inside? {}", rect1.can_hold(&methodRect));
+    println!("Can rect2 hold rect1 inside? {}", methodRect.can_hold(&rect1));
 }
 
 // This provides a more descriptive understanding of the code
